521343b83f2f4f6a5f8ab936862bd0523547c4f9
drobin45 <drobin45@kent.edu>
2017-08-23 13:51:38 -0400

    Attempting to split original reading.txt into seperate chars

--- HowManyHoles/Program.cs
+++ HowManyHoles/Program.cs
@@ -15,53 +15,46 @@ namespace HowManyHoles
             //Console.WriteLine(reader.ReadToEnd());
             //Console.WriteLine(reader.ReadLine());
 
-            string line = reader.ReadLine();
-            Console.WriteLine(line);
-            int holes = 0;
+            string line = reader.ReadLine();            
+            Console.WriteLine("Original lines: " + line);
 
-            //Console.WriteLine(line[0]);
-            int i;        
-                do
+            char[] splitLine = line.ToCharArray();
+            Console.WriteLine("First digit of problem: " + splitLine[0]);
+
+            int holes = 0;
+            Console.WriteLine("Original holes: " + holes);
+
+            //Potential problem: line[i] may be an int, while all case '#' are chars, so even when line[i] has a 4, it will not register as '4'     
+            int i;
+            
+                for (i = 0; i >= splitLine.Length; i++)
                 {
-                for (i = 0; i < line.Length; i++)
-                {
-                    Console.WriteLine(line[i]);
-                    switch (line[i])
+                    char number = splitLine[i];
+                    Console.WriteLine("Testing: should be 1: "+number);
+                    switch (splitLine[i])
                     {                        
                         case '4':
-                            holes += 1;
+                            holes++;
                             break;                     
                         case '6':
-                            holes += 1;
+                            holes++;
                             break;                        
                         case '8':
                             holes += 2;
                             break;
                         case '9':
-                            holes += 1;
+                            holes++;
                             break;
                         case '0':
-                            holes += 1;
+                            holes++;
                             break;
                         default:
-                            break;
-                    }
-                    
-                } 
-               Console.WriteLine(holes);
-            } while (i == line.Length);
-
-            //string[] lineSplit = line.Split();
-            //foreach (string numbers in lineSplit)
-            //{
-            //    Console.WriteLine(numbers);
-            //}
-
-            //string[] test = { "1", "2", "3", "4" };
-            //foreach (string number in test)
-            //{
-            //    Console.WriteLine(number);
-            //}
+                            continue;
+                    }                    
+                }
+                Console.WriteLine("Number of holes in lines " + holes);
+            
+            
 
 
 
