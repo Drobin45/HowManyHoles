3321b3b0ae22be75e7bd5d5a1b4373ac590cb33b
drobin45 <drobin45@kent.edu>
2017-08-23 14:09:29 -0400

    Finished problem successfully

--- HowManyHoles/Program.cs
+++ HowManyHoles/Program.cs
@@ -15,23 +15,22 @@ namespace HowManyHoles
             //Console.WriteLine(reader.ReadToEnd());
             //Console.WriteLine(reader.ReadLine());
 
-            string line = reader.ReadLine();            
-            Console.WriteLine("Original lines: " + line);
+            string line = reader.ReadToEnd();            
+            Console.WriteLine("Original lines: \n" + line);
 
             char[] splitLine = line.ToCharArray();
-            Console.WriteLine("First digit of problem: " + splitLine[0]);
+            //Console.WriteLine("First digit of problem: " + splitLine[0]);
 
             int holes = 0;
-            Console.WriteLine("Original holes: " + holes);
+            Console.WriteLine("Holes read before application: " + holes);
 
             //Potential problem: line[i] may be an int, while all case '#' are chars, so even when line[i] has a 4, it will not register as '4'     
-            int i;
-            
-                for (i = 0; i >= splitLine.Length; i++)
+            int i;            
+                for (i = 0; i < splitLine.Length; i++)
                 {
                     char number = splitLine[i];
-                    Console.WriteLine("Testing: should be 1: "+number);
-                    switch (splitLine[i])
+                    //Console.WriteLine("Testing: should be 1: " + number);
+                    switch (number)
                     {                        
                         case '4':
                             holes++;
